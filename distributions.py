a9e188b59878f58261497db4d9cfc5ee62c96542
Soumya Basu <soumyab@berkeley.edu>
2014-12-10 14:00:56 -0800

    Distribution generators added

---
+++ distributions.py
@@ -0,0 +1,14 @@
+from scipy.stats import genextreme, genpareto
+
+
+def key_distribution(num_samples):
+    dist = genextreme(30.7984, 8.20449, 0.078688)
+    return dist.rvs(num_samples)
+
+def value_distribution(num_samples):
+    dist = genpareto(0, 214.476, 0.348238)
+    return dist.rvs(num_samples)
+
+def inter_arrival_dist(num_samples):
+    dist = genpareto(0, 16.0292, 0.154971)
+    return dist.rvs(num_samples)
