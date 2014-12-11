0c0edc058483eed2c0fb98c39554f53ea3cddaaf
Sagar Karandikar <karandikarsagar@gmail.com>
2014-12-10 23:04:02 -0800

    stuff

--- memcached_set.py
+++ memcached_set.py
@@ -4,13 +4,18 @@ mc = pylibmc.Client(["172.16.1.2"])
 
 
 def load():
+    res = False
+    while not res:
+        res = mc.set("a", "0")
+
     for x in range(200):
         res = False
         while not res:
-            res = mc.set(str(x) + "hello", str(x) + "hello")
-        print("sending (" + str(x) + "hello, " + str(x) + "hello)")
+            res = mc.set(str(x) + "hello", str(x) + "hello2")
+        print("sending (" + str(x) + "hello, " + str(x) + "hello2)")
 
 
 def check():
+    mc['a']
     for x in range(100):
         mc[str(x)+"hello"]
